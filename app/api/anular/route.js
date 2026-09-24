77e304a782c67d5ba5535b3b41b56678461bd142
Alerzyo2003 <avengerzdarkness@gmail.com>
2026-09-24 17:51:16 -0300

    Update route.js

--- app/api/anular/route.js
+++ app/api/anular/route.js
@@ -9,7 +9,6 @@ import {
 
 export async function GET() {
   try {
-    // Inicializamos con las mismas credenciales públicas de prueba que usaste para crear el pago
     const tx = new WebpayPlus.Transaction(
       new Options(
         IntegrationCommerceCodes.WEBPAY_PLUS,
@@ -18,7 +17,7 @@ export async function GET() {
       )
     )
 
-    // Tus dos tokens reales y frescos
+    // Tus dos tokens listos para ser anulados
     const TOKEN_PARCIAL = '01ab22f82d4319e2fc9256ba131c257b0ae3bdb179ba12e9a6ee879d0d8ec3c9'
     const TOKEN_TOTAL = '01ab4bb98ae6761cd2e062e3793d23db0a93271b82156e8247333bedd380442f'
 
@@ -38,7 +37,7 @@ export async function GET() {
       detalle: { parcialResponse, totalResponse }
     })
 
-  } catch (error: any) {
+  } catch (error) {
     console.error('Error al anular en Transbank:', error)
     return NextResponse.json({ 
       success: false, 
